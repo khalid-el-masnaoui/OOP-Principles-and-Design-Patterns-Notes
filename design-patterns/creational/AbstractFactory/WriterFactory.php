91aa523dc6cdc09c44b5be75364ebd89041a0bb1
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-10-13 06:25:30 +0100

    OOP design patterns : abstract factory -> example-2 : writer factory

---
+++ design-patterns/creational/AbstractFactory/WriterFactory.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace DesignPatterns\Creational\AbstractFactory;
+
+/**
+ * Abstract Factory defines an interface for creating all distinct products,
+ * but leaves the actual product creation to concrete factory classes
+ */
+interface WriterFactory
+{
+    public function createCsvWriter(): CsvWriter;
+    public function createJsonWriter(): JsonWriter;
+}
+
+/**
+ * Each factory type corresponds to a certain product variety
+ */
+ class UnixWriterFactory implements WriterFactory
+{
+    public function createCsvWriter(): CsvWriter
+    {
+        return new UnixCsvWriter();
+    }
+
+    public function createJsonWriter(): JsonWriter
+    {
+        return new UnixJsonWriter();
+    }
+}
+
+class WinWriterFactory implements WriterFactory
+{
+    public function createCsvWriter(): CsvWriter
+    {
+        return new WinCsvWriter();
+    }
+
+    public function createJsonWriter(): JsonWriter
+    {
+        return new WinJsonWriter();
+    }
+}
+
+/**
+ * The base interface for csvWriter (products) family
+ */
+ interface CsvWriter
+{
+    public function write(array $line): string;
+}
+
+class UnixCsvWriter implements CsvWriter
+{
+    public function write(array $line): string
+    {
+        return join(',', $line) . "\n";
+    }
+}
+
+class WinCsvWriter implements CsvWriter
+{
+    public function write(array $line): string
+    {
+        return join(',', $line) . "\r\n";
+    }
+}
+
+
+/**
+ * Another products family
+ */
+interface JsonWriter
+{
+    public function write(array $data, bool $formatted): string;
+}
+
+
+class UnixJsonWriter implements JsonWriter
+{
+    public function write(array $data, bool $formatted): string
+    {
+        $options = 0;
+
+        if ($formatted) {
+            $options = JSON_PRETTY_PRINT;
+        }
+
+        return json_encode($data, $options);
+    }
+}
+
+class WinJsonWriter implements JsonWriter
+{
+    public function write(array $data, bool $formatted): string
+    {
+        $options = 0;
+
+        if ($formatted) {
+            $options = JSON_PRETTY_PRINT;
+        }
+
+        return json_encode($data, $options);
+    }
+}
+
+# Client code example
+// the factory is selected based on the environment or configuration parameters
+$writerType = 'unix';
+switch ($writerType) {
+    case 'unix':
+        $writerFactory = new UnixWriterFactory();
+        break;
+    case 'win':
+        $writerFactory = new WinWriterFactory();
+        break;
+}
+
+// we will have csv and json written as in either Unix or Win format, but never mixed
+echo $writerFactory->createCsvWriter()->write(array('item1', 'item2', 'item3'));
+echo $writerFactory->createJsonWriter()->write(array('item1' => 'chair', 'item2' => 'table', 'item3' => 'sofa'));
+/* Output: 
+ item1,item2,item3
+ {
+    'item1' : 'chair',
+    'item2' : 'table',
+    'item3' : 'sofa'
+ }
+ */
+
+
+
+
