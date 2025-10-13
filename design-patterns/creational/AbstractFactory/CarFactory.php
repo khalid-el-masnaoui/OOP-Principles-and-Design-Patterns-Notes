d897bc1bc3a2fabd69a5fc1d7b1cd95e782d2f7e
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2025-10-13 07:07:39 +0100

    OOP design patterns : abstract factory -> example-3 : car factory

---
+++ design-patterns/creational/AbstractFactory/CarFactory.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace DesignPatterns\Creational;
+
+/**
+ * Abstract Factory defines an interface for creating all distinct products,
+ * but leaves the actual product creation to concrete factory classes
+ */
+abstract class CarFactory
+{
+    abstract public function build();
+}
+
+/**
+ * Each factory type corresponds to a certain product variety
+ */
+class RedCarFactory extends CarFactory
+{
+    private $options;
+
+    public function __construct($doors)
+    {
+        $options = new CarOptions();
+        $options->color = "Red";
+        $options->doors = $doors;
+
+        $this->options = $options;
+    }
+
+    public function build()
+    {
+        return new Car($this->options);
+    }
+}
+
+class BlueCarFactory extends CarFactory
+{
+    private $options;
+
+    public function __construct($doors)
+    {
+        $options = new CarOptions();
+        $options->color = "Blue";
+        $options->doors = $doors;
+
+        $this->options = $options;
+    }
+
+    public function build()
+    {
+        return new Car($this->options);
+    }
+}
+ 
+ 
+class Car
+{
+    private $color;
+    private $doors;
+
+    public function __construct(CarOptions $options)
+    {
+        $this->color = $options->color;
+        $this->doors = $options->doors;
+    }
+
+    public function info()
+    {
+        return "This is a $this->doors doors $this->color car.\n";
+    }
+}
+
+class CarOptions
+{
+    var $color = "White";
+    var $doors = 4;
+}
+
+# Client code example
+// the factory is selected based on the environment or configuration parameters
+$carColor = 'blue';
+switch ($carColor) {
+    case 'blue':
+        $carFactory = new BlueCarFactory(4);
+        break;
+    case 'red':
+        $carFactory = new RedCarFactory(4);
+        break;
+}
+
+// we will have header and body as either Smarty or Blade template, but never mixed
+echo $carFactory->build()->info();
+/* Output: This is a 5 doors Blue car. */
+
